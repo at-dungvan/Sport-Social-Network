4db3ebcc31952da92bde684215cead7a970e0d04
at-dungvan <dungvan2512@gmail.com>
2017-10-28 00:12:18 +0700

    Update 2017_10_25_152319_create_teams_table.php

--- src/database/migrations/2017_10_25_152319_create_teams_table.php
+++ src/database/migrations/2017_10_25_152319_create_teams_table.php
@@ -16,7 +16,7 @@ class CreateTeamsTable extends Migration
         Schema::create('teams', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->integer('team_master_id');
+            $table->integer('team_master_id')->unsigned();
             $table->timestamps();
 
             $table->foreign('team_master_id')->references('id')->on('users');
